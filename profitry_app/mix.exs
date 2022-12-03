fc097d612f2a8183898387b9964e424731ca01f4
Rui Ferrão <rui@ferrao.dev>
2022-12-03 20:45:38 +0000

    organized mix deps, dep updates, security audit tool

--- profitry_app/mix.exs
+++ profitry_app/mix.exs
@@ -37,19 +37,24 @@ defmodule ProfitryApp.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.3"},
       {:heroicons, "~> 0.5"},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.7.2"},
-      {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
+
+      # Dev Deps
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
+      {:sobelow, "~> 0.8", only: :dev},
+
+      # Test Deps
+      {:floki, ">= 0.30.0", only: :test},
 
       # Local Deps
       {:profitry_server, path: "../profitry_server"}
