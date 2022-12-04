16ee25d1d2dfb9b69adaf9f58f6ea7629bf105e6
Rui Ferrão <rui@ferrao.dev>
2022-12-04 01:27:41 +0000

    authentication

--- profitry_app/lib/profitry_app_web/router.ex
+++ profitry_app/lib/profitry_app_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ProfitryAppWeb.Router do
   use ProfitryAppWeb, :router
 
+  import ProfitryAppWeb.UserAuth
+
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
@@ -8,6 +10,7 @@ defmodule ProfitryAppWeb.Router do
     plug(:put_root_layout, {ProfitryAppWeb.Layouts, :root})
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug(:fetch_current_user)
   end
 
   pipeline :api do
@@ -18,13 +21,6 @@ defmodule ProfitryAppWeb.Router do
     pipe_through(:browser)
 
     get("/", PageController, :home)
-
-    live("/portfolios", PortfolioLive.Index, :index)
-    live("/portfolios/new", PortfolioLive.Index, :new)
-    live("/portfolios/:id/edit", PortfolioLive.Index, :edit)
-
-    live("/portfolios/:id", PortfolioLive.Show, :show)
-    live("/portfolios/:id/show/edit", PortfolioLive.Show, :edit)
   end
 
   # Other scopes may use custom stacks.
@@ -46,6 +42,50 @@ defmodule ProfitryAppWeb.Router do
 
       live_dashboard("/dashboard", metrics: ProfitryAppWeb.Telemetry)
       forward("/mailbox", Plug.Swoosh.MailboxPreview)
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ProfitryAppWeb do
+    pipe_through([:browser, :redirect_if_user_is_authenticated])
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ProfitryAppWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live("/users/register", UserRegistrationLive, :new)
+      live("/users/log_in", UserLoginLive, :new)
+      live("/users/reset_password", UserForgotPasswordLive, :new)
+      live("/users/reset_password/:token", UserResetPasswordLive, :edit)
+    end
+
+    post("/users/log_in", UserSessionController, :create)
+  end
+
+  scope "/", ProfitryAppWeb do
+    pipe_through([:browser, :require_authenticated_user])
+
+    live_session :require_authenticated_user,
+      on_mount: [{ProfitryAppWeb.UserAuth, :ensure_authenticated}] do
+      live("/users/settings", UserSettingsLive, :edit)
+      live("/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email)
+
+      live("/portfolios", PortfolioLive.Index, :index)
+      live("/portfolios/new", PortfolioLive.Index, :new)
+      live("/portfolios/:id", PortfolioLive.Show, :show)
+      live("/portfolios/:id/edit", PortfolioLive.Index, :edit)
+      live("/portfolios/:id/show/edit", PortfolioLive.Show, :edit)
+    end
+  end
+
+  scope "/", ProfitryAppWeb do
+    pipe_through([:browser])
+
+    delete("/users/log_out", UserSessionController, :delete)
+
+    live_session :current_user,
+      on_mount: [{ProfitryAppWeb.UserAuth, :mount_current_user}] do
+      live("/users/confirm/:token", UserConfirmationLive, :edit)
+      live("/users/confirm", UserConfirmationInstructionsLive, :new)
     end
   end
 end
