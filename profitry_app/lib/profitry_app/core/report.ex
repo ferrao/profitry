bb56b61735548edfa9b73d5973082bfd9c6e35fa
Rui Ferrão <rui@ferrao.dev>
2022-12-11 20:25:29 +0000

    position reports

--- profitry_app/lib/profitry_app/core/report.ex
+++ profitry_app/lib/profitry_app/core/report.ex
@@ -1,7 +1,7 @@
 defmodule ProfitryApp.Core.Report do
   use Ecto.Schema
 
-  alias ProfitryApp.Core.Position
+  alias ProfitryApp.Core.{Position, Order, Report}
 
   schema "reports" do
     field :ticker, :string
@@ -10,13 +10,114 @@ defmodule ProfitryApp.Core.Report do
     field :cost_basis, :decimal
   end
 
-  def make_report(%Position{} = position) do
-    %__MODULE__{
-      id: position.id,
-      ticker: position.ticker,
+  def make_report(%Position{id: id, ticker: ticker, orders: orders}) do
+    report = %Report{
+      id: id,
+      ticker: ticker,
       investment: 0,
       shares: 0,
       cost_basis: 0
     }
+
+    report = Enum.reduce(orders, report, fn order, report -> calculate_order(report, order) end)
+
+    has_shares = Decimal.gt?(report.shares, 0)
+
+    report
+    |> calculate_cost_basis(has_shares)
+    |> stringify_decimals
+    |> Map.put(:ticker, ticker)
+    |> IO.inspect()
+  end
+
+  # calculates impact of buying shares on an existing position
+  defp calculate_order(report, %Order{
+         type: :buy,
+         instrument: :stock,
+         quantity: quantity,
+         price: price
+       }) do
+    %Report{
+      report
+      | investment: Decimal.add(report.investment, Decimal.mult(quantity, price)),
+        shares: Decimal.add(report.shares, quantity)
+    }
+  end
+
+  # calculates impact of selling shares on an existing position
+  defp calculate_order(report, %Order{
+         type: :sell,
+         instrument: :stock,
+         quantity: quantity,
+         price: price
+       }) do
+    %Report{
+      report
+      | investment: Decimal.sub(report.investment, Decimal.mult(quantity, price)),
+        shares: Decimal.sub(report.shares, quantity)
+    }
+  end
+
+  # calculates impact of buying premium on an existing position
+  defp calculate_order(report, %Order{
+         type: :buy,
+         instrument: :option,
+         quantity: quantity,
+         price: price
+       }) do
+    %Report{
+      report
+      | investment:
+          Decimal.add(
+            report.investment,
+            quantity
+            |> Decimal.mult(price)
+            |> Decimal.mult(100)
+          )
+    }
+  end
+
+  # calculates impact of selling premium on an existing position
+  defp calculate_order(report, %Order{
+         type: :sell,
+         instrument: :option,
+         quantity: quantity,
+         price: price
+       }) do
+    %Report{
+      report
+      | investment:
+          Decimal.sub(
+            report.investment,
+            quantity
+            |> Decimal.mult(price)
+            |> Decimal.mult(100)
+          )
+    }
+  end
+
+  # calculates the cost basis for a position report with no shares
+  defp calculate_cost_basis(report, _has_shares = false) do
+    Map.put(report, :cost_basis, "0.00")
+  end
+
+  # calculates the cost basis for a position report with shares
+  defp calculate_cost_basis(report, _has_shares) do
+    Map.put(report, :cost_basis, Decimal.div(report.investment, report.shares))
+  end
+
+  defp stringify_decimals(report) do
+    %Report{
+      report
+      | investment: decimal_to_string(report.investment),
+        shares: decimal_to_string(report.shares),
+        cost_basis: decimal_to_string(report.cost_basis)
+    }
+  end
+
+  defp decimal_to_string(decimal) do
+    decimal
+    |> Decimal.round(2)
+    |> Decimal.to_string()
   end
 end
