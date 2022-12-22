d0f5a1bf3eb0b7f7b005f3182b232519adf1ea22
Rui Ferrão <rui@ferrao.dev>
2022-12-22 01:37:34 +0000

    updated hostname for custom domain

--- profitry_app/config/runtime.exs
+++ profitry_app/config/runtime.exs
@@ -49,7 +49,7 @@ if config_env() == :prod do
       """
 
   # host = System.get_env("PHX_HOST") || "example.com"
-  host = System.get_env("APP_NAME") <> ".gigalixirapp.com"
+  host = System.get_env("HOST")
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :profitry_app, ProfitryAppWeb.Endpoint,
