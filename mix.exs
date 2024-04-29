7918a427fa2c047e06bf8352ba864c2ce7ceae19
Rui Ferrão <rui@ferrao.dev>
2024-04-29 18:06:34 +0100

    added dialyxir dep

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Profitry.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
