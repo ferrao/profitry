1a1b35312c4b729768c582af62292f461661c704
Rui Ferrão <rui@ferrao.dev>
2024-12-13 23:54:09 +0000

    source config from env variables

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :profitry, Profitry.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "profitry_dev",
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASS"),
+  hostname: System.get_env("DB_HOST"),
+  database: System.get_env("DB_NAME"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
