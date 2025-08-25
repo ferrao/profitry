c7b25ae7f6e30d9453d96bef6fa1ac3368afa46c
Rui Ferrão <rui@ferrao.dev>
2025-08-25 15:25:14 +0100

    ticker changes crud

---
+++ lib/profitry/investment/ticker_changes.ex
@@ -0,0 +1,103 @@
+defmodule Profitry.Investment.TickerChanges do
+  @moduledoc """
+
+  Operations on TickerChanges struct
+
+  """
+
+  alias Ecto.Changeset
+  alias Profitry.Repo
+  alias Profitry.Investment.Schema.TickerChange
+
+  @doc """
+
+  Creates a ticker change event
+
+  ## Examples
+
+      iex> create_ticker_change(%{field: value})
+      {:ok, %TickerChange{}}
+
+      iex> create_ticker_change(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  @spec create_ticker_change(map()) :: {:ok, TickerChange.t()}
+  def create_ticker_change(attrs \\ %{}) do
+    %TickerChange{}
+    |> TickerChange.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+
+  Returns the list of ticker changes
+
+  ## Examples
+
+      iex> list_ticker_changes()
+      [%TickerChange{}, ...]
+
+  """
+  @spec list_ticker_changes() :: list(TickerChange.t())
+  def list_ticker_changes() do
+    Repo.all(TickerChange)
+  end
+
+  @doc """
+
+  Updates a ticker change event
+
+  ## Examples
+
+      iex> update_ticker_change(ticker_change, %{field: new_value})
+      {:ok, %Tickerchange{}}
+
+      iex> update_split(split, %{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  @spec update_ticker_change(TickerChange.t(), map()) ::
+          {:ok, TickerChange.t()} | {:error, Changeset.t()}
+  def update_ticker_change(%TickerChange{} = ticker_change, attrs) do
+    ticker_change
+    |> TickerChange.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+
+  Deletes a ticker change.
+
+  ## Examples
+
+      iex> delete_ticker_change(ticker_change)
+      {:ok, %TickerChange{}}
+
+      iex> delete_ticker_change(ticker_change)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  @spec delete_ticker_change(TickerChange.t()) ::
+          {:ok, TickerChange.t()} | {:error, Changeset.t()}
+  def delete_ticker_change(%TickerChange{} = ticker_change) do
+    ticker_change
+    |> change_ticker_change()
+    |> Repo.delete()
+  end
+
+  @doc """
+
+  Returns an `%Ecto.Changeset{}` for tracking stock ticker changes.
+
+  ## Examples
+
+      iex> change_ticker_change(ticker_change)
+      %Ecto.Changeset{data: %TickerChange{}}
+
+  """
+  @spec change_ticker_change(TickerChange.t(), map()) :: Changeset.t()
+  def change_ticker_change(%TickerChange{} = ticker_change, attrs \\ %{}) do
+    TickerChange.changeset(ticker_change, attrs)
+  end
+end
---
+++ test/profitry/investment/ticker_changes_test.exs
@@ -0,0 +1,65 @@
+defmodule Profitry.Investment.TickerChangesTest do
+  use Profitry.DataCase, async: true
+
+  import Profitry.InvestmentFixtures
+
+  alias Profitry.Investment
+  alias Profitry.Investment.Schema.TickerChange
+
+  describe "ticker changes" do
+    test "create_ticker_change/1 with valid data creates a ticker change" do
+      attrs = %{
+        ticker: "aapl",
+        original_ticker: "aaaa",
+        date: "2024-01-01"
+      }
+
+      assert {:ok, %TickerChange{} = ticker_change} = Investment.create_ticker_change(attrs)
+      assert ticker_change.ticker === String.upcase(attrs.ticker)
+      assert ticker_change.original_ticker === String.upcase(attrs.original_ticker)
+      assert ticker_change.date === Date.from_iso8601!(attrs.date)
+      assert ticker_change === Repo.get!(TickerChange, ticker_change.id)
+    end
+
+    test "create_ticker_change/1 with invalid data creates an error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Investment.create_ticker_change(%{})
+    end
+
+    test "list_ticker_changes/0 returns existing ticker changes" do
+      ticker_change = ticker_change_fixture()
+
+      assert Investment.list_ticker_changes() === [ticker_change]
+    end
+
+    test "update_ticker_change/2 with valid data updates a ticker change" do
+      ticker_change = ticker_change_fixture()
+      attrs = %{ticker: "bbbb", original_ticker: "aaaa"}
+
+      assert {:ok, %TickerChange{} = updated_ticker_change} =
+               Investment.update_ticker_change(ticker_change, attrs)
+
+      assert updated_ticker_change.ticker === String.upcase(attrs.ticker)
+      assert updated_ticker_change.original_ticker === String.upcase(attrs.original_ticker)
+    end
+
+    test "update_ticker_change/2 with invalid data does not update a ticker change" do
+      ticker_change = ticker_change_fixture()
+      attrs = %{ticker: "appl", original_ticker: "appl"}
+
+      assert {:error, %Ecto.Changeset{}} = Investment.update_ticker_change(ticker_change, attrs)
+    end
+
+    test "change_ticker_change/1 returns a ticker_change changeset" do
+      ticker_change = ticker_change_fixture()
+
+      assert %Ecto.Changeset{} = Investment.change_ticker_change(ticker_change)
+    end
+
+    test "delete_ticker_change/1 deletes ticker change" do
+      ticker_change = ticker_change_fixture()
+
+      assert {:ok, %TickerChange{}} = Investment.delete_ticker_change(ticker_change)
+      assert_raise Ecto.NoResultsError, fn -> Repo.get!(TickerChange, ticker_change.id) end
+    end
+  end
+end
